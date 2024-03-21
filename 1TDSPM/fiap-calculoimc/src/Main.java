e96c532828bb62dcdbfa24e26bc4b4867ef75c49
Erick0105 <erickspot01@gmail.com>
2024-03-21 09:30:15 -0300

    receber variaveis

--- 1TDSPM/fiap-calculoimc/src/Main.java
+++ 1TDSPM/fiap-calculoimc/src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
 
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu peso: ");
+        double peso = leitor.nextDouble();
+        System.out.println("Digite sua altura");
+        double altura = leitor.nextDouble();
+
+        
     }
 }
