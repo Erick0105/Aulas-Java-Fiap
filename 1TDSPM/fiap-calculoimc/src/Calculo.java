ff7d5254ae453d482d448aba7cfd8c3f2a3b9ac4
Erick0105 <erickspot01@gmail.com>
2024-03-21 09:09:20 -0300

    metodo exibirMensagem

---
+++ 1TDSPM/fiap-calculoimc/src/Calculo.java
@@ -0,0 +1,35 @@
+public class Calculo {
+
+    //Atributos
+    public double peso;
+    public double altura;
+
+    //Método construtor
+    public Calculo(double peso, double altura){
+        this.peso = peso;
+        this.altura = altura;
+
+        //double x = Math.pow(3,2) 3²;
+    }
+
+    //Métodos
+    public double calcularImc(){
+        return this.peso /Math.pow(this.altura,2);
+    }
+    public String exibirMensagem(){
+        String mensagem;
+        if (this.calcularImc() <= 18.5)
+            mensagem = "Seu IMC é de " + calcularImc() + "e você está abaixo do peso.";
+        else if (this.calcularImc() < 24.9 && this.calcularImc() >= 18.6)
+            mensagem = "Seu IMC é de " + calcularImc() + "e você está no Peso ideal(parabéns)";
+        else if (this.calcularImc() < 29.9 && this.calcularImc() >= 25)
+            mensagem = "Seu IMC é de " + calcularImc() + "e você está levemente acima do peso";
+        else if (this.calcularImc() < 34.9 && this.calcularImc() >= 30)
+            mensagem = "Seu IMC é de " + calcularImc() + "e você está em obesidade grau 1";
+        else if (this.calcularImc() < 39.9 && this.calcularImc() >= 35)
+            mensagem = "Seu IMC é de " + calcularImc() + "e você está em obesidade grau 2";
+        else
+            mensagem = "Seu IMC é de " + calcularImc() + "e você está em obesidade grau 3";
+        return mensagem;
+    }
+}
