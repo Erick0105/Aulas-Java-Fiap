df91cf97a6b83b1ea12285c114dae459b9f3b9fd
Erick0105 <erickspot01@gmail.com>
2024-03-21 18:16:20 -0300

    metodo construtor triangulo

---
+++ myself/untitled/src/Triangulo.java
@@ -0,0 +1,11 @@
+public class Triangulo {
+
+    public double base;
+    public double altura;
+
+    public Triangulo(double base, double altura){
+        this.base = base;
+        this.altura = altura;
+    }
+
+}
