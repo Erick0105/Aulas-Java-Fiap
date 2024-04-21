bef3a3359a3aa8a3c86f1c9c524787916304c727
Erick0105 <erickspot01@gmail.com>
2024-04-21 13:40:32 -0300

    estrutura básica main

--- ControleDespesa/src/GerenciadorDespesas.java
+++ ControleDespesa/src/GerenciadorDespesas.java
@@ -1,5 +1,6 @@
 public class GerenciadorDespesas {
 
+    //ATRIBUTOS
     private int qtdeAlimentacao;
     private int qtdeTransporte;
     private int qtdeDiaria;
@@ -8,59 +9,55 @@ public class GerenciadorDespesas {
     private double totalDiaria;
     private double totalDespesas;
 
+
+    //GETTERS E SETTERS
     public int getQtdeAlimentacao() {
         return qtdeAlimentacao;
+    }
+    public int getQtdeTransporte() {
+        return qtdeTransporte;
+    }
+    public int getQtdeDiaria() {
+        return qtdeDiaria;
+    }
+    public double getTotalAlimentacao() {
+        return totalAlimentacao;
+    }
+    public double getTotalTransporte() {
+        return totalTransporte;
+    }
+    public double getTotalDiaria() {
+        return totalDiaria;
+    }
+    public double getTotalDespesas() {
+        return totalDespesas;
     }
 
     public void setQtdeAlimentacao(int qtdeAlimentacao) {
         this.qtdeAlimentacao = qtdeAlimentacao;
     }
-
-    public int getQtdeTransporte() {
-        return qtdeTransporte;
-    }
-
     public void setQtdeTransporte(int qtdeTransporte) {
         this.qtdeTransporte = qtdeTransporte;
     }
-
-    public int getQtdeDiaria() {
-        return qtdeDiaria;
-    }
-
     public void setQtdeDiaria(int qtdeDiaria) {
         this.qtdeDiaria = qtdeDiaria;
     }
-
-    public double getTotalAlimentacao() {
-        return totalAlimentacao;
-    }
-
     public void setTotalAlimentacao(double totalAlimentacao) {
         this.totalAlimentacao = totalAlimentacao;
     }
-
-    public double getTotalTransporte() {
-        return totalTransporte;
-    }
-
     public void setTotalTransporte(double totalTransporte) {
         this.totalTransporte = totalTransporte;
     }
-
-    public double getTotalDiaria() {
-        return totalDiaria;
-    }
-
     public void setTotalDiaria(double totalDiaria) {
         this.totalDiaria = totalDiaria;
     }
-
-    public double getTotalDespesas() {
-        return totalDespesas;
-    }
-
     public void setTotalDespesas(double totalDespesas) {
         this.totalDespesas = totalDespesas;
+    }
+
+
+    //METÓDOS
+    public void analisarDespesas(Despesa despesa){
+
     }
 }
--- ControleDespesa/src/Principal.java
+++ ControleDespesa/src/Principal.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        //OBJETOS INSTANCIADOS
+        Alimentacao refeicoes = new Alimentacao();
+        Transporte veiculos = new Transporte();
+        Diaria diarias = new Diaria();
+        GerenciadorDespesas gerenciador = new GerenciadorDespesas();
+
+        //FUNCIONALIDADE
+        System.out.println("---------------MENU---------------" +
+                "\n[1] - Alimentação" +
+                "\n[2] - Transporte" +
+                "\n[3] - Diaria" +
+                "\n[4] - Apresenta analise de despesas" +
+                "\n[0] - Sair" +
+                "\n----------------------------------");
+
+
+        System.out.println("---------------MENU---------------" +
+                "\n[1] - Cadastrar despesa" +
+                "\n[2] - Calcular despesa" +
+                "\n[3] - Apresentar despesa" +
+                "\n[4] - Incluir despesa para analise" +
+                "\n[0] - Sair" +
+                "\n----------------------------------");
+
     }
 }
