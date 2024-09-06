a6294f47380792435a771081b283a31159fe172c
Erick0105 <erickaxs0105@gmail.com>
2024-09-06 09:42:35 -0300

    Crud buildado

--- ddd-cliente/src/main/java/br/com/fiap/cliente/model/Endereco.java
+++ ddd-cliente/src/main/java/br/com/fiap/cliente/model/Endereco.java
@@ -9,5 +9,62 @@ public class Endereco {
     private String estado;
     private String regiao;
     private int ddd;
-    
+
+    //Getters e Setters
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNmRua() {
+        return nmRua;
+    }
+    public void setNmRua(String nmRua) {
+        this.nmRua = nmRua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
 }
---
+++ ddd-cliente/src/test/java/br/com/fiap/cliente/TestPrincipal.java
@@ -0,0 +1,16 @@
+package br.com.fiap.cliente;
+
+import java.util.Scanner;
+
+public class TestPrincipal {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("-----------OPÇÕES------------" +
+                "\n[1] - Cadastrar;" +
+                "\n[2] - Listar;" +
+                "\n[3] - Pesquisar por Cep;" +
+                "\n\nDigite o número correspondente a opção desejada");
+        int opCrud = leitor.nextInt();
+    }
+}
