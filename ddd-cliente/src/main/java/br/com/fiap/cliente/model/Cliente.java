7a2b9b2c528c2daec732ce1cce0d609105daf462
Erick0105 <erickaxs0105@gmail.com>
2024-09-06 10:02:39 -0300

    Classe cliente construida

--- ddd-cliente/src/main/java/br/com/fiap/cliente/model/Cliente.java
+++ ddd-cliente/src/main/java/br/com/fiap/cliente/model/Cliente.java
@@ -1,4 +1,70 @@
 package br.com.fiap.cliente.model;
 
+import java.util.Scanner;
+
 public class Cliente {
+    Scanner leitor = new Scanner(System.in);
+
+    //Atributos
+    private String nome;
+    private int idade;
+    private int cpf;
+    private String email;
+    private int telefone;
+
+    //Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    //Metodos
+    public Cliente CadastrarCliente(){
+        System.out.println("Qual o nome do Cliente?");
+        this.nome = leitor.next();
+
+        System.out.println("Qual a idade do cliente?");
+        this.idade = leitor.nextInt();
+
+        System.out.println("Qual o cpf do cliente?");
+        this.cpf = leitor.nextInt();
+
+        System.out.println("Qual o email do Cliente?");
+        this.email = leitor.next();
+
+        System.out.println("Qual o telefone do Cliente?");
+        this.telefone = leitor.nextInt();
+        return this;
+    }
 }
--- ddd-cliente/src/test/java/br/com/fiap/cliente/TestPrincipal.java
+++ ddd-cliente/src/test/java/br/com/fiap/cliente/TestPrincipal.java
@@ -7,10 +7,18 @@ public class TestPrincipal {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("-----------OPÇÕES------------" +
+                "\n[0] - Sair;" +
                 "\n[1] - Cadastrar;" +
                 "\n[2] - Listar;" +
-                "\n[3] - Pesquisar por Cep;" +
+                "\n[3] - Pesquisar por Cep." +
                 "\n\nDigite o número correspondente a opção desejada");
         int opCrud = leitor.nextInt();
+
+        switch (opCrud){
+            case 1:
+            case 2:
+            case 3:
+            default:
+        }
     }
 }
