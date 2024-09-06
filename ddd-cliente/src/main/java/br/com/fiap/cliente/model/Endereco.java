9f0c64cc86ba12967457359e62489dfdff800094
Erick0105 <erickaxs0105@gmail.com>
2024-09-06 09:37:04 -0300

    usando maven para criar arquivos

---
+++ ddd-cliente/src/main/java/br/com/fiap/cliente/model/Endereco.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cliente.model;
+
+public class Endereco {
+    private Endereco endereco;
+    private String cep;
+    private String nmRua;
+    private String complemento;
+    private String bairro;
+    private String estado;
+    private String regiao;
+    private int ddd;
+    
+}
