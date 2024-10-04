7cf1d5836d9b0fc6e53e5b6d98e8635d4f698ab1
Erick0105 <erickaxs0105@gmail.com>
2024-10-04 09:07:34 -0300

    projeto todo

---
+++ webapp/src/main/java/br/com/fiap/web/service/ProdutoService.java
@@ -0,0 +1,26 @@
+package br.com.fiap.web.service;
+
+import br.com.fiap.web.dao.ProdutoDao;
+import br.com.fiap.web.model.Produto;
+
+import java.util.List;
+
+public class ProdutoService {
+
+    private ProdutoDao produtoDao = new ProdutoDao();
+
+    public List<Produto> listar(){
+        return produtoDao.listar();
+    }
+
+
+    //Retornar um único produto -> buscarPorId
+    public Produto buscarPorId(int codigo){
+        return produtoDao.buscarPorId(codigo);
+    }
+
+
+    public Produto cadastrar(Produto produto){
+        return produtoDao.inserir(produto);
+    }
+}
